f02f49aed962c44b3971b50d94a2a9dea0ba440d
Daniel Strutt <31758577+DanielStrutt@users.noreply.github.com>
2017-09-19 22:12:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NameFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title;
+            string forename;
+            string surname;
+            string end;
+            Console.WriteLine("Please Enter Your Title.");
+            title = Console.ReadLine();
+            Console.WriteLine("Please Enter Your Forename.");
+            forename = Console.ReadLine();
+            Console.WriteLine("Please Enter Your Surname.");
+            surname = Console.ReadLine();
+            Console.WriteLine("Ah I see that your name is {0} {1} {2}, how interesting. Press any key to exit.",title,forename,surname);
+            end = Console.ReadLine();
+            Console.WriteLine("Program now ending.");
+        }
+    }
+}
