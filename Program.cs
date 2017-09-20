71e2a440727e8c3687d842a963761f2ab573390d
Daniel Strutt <31758577+DanielStrutt@users.noreply.github.com>
2017-09-20 04:59:26 +0100

    Add files via upload
    Source code and console application for question 4.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)//Function which takes a string as a parameter and capitalises the first letter of each word. 
+        {
+            string sentence;
+            string end;
+ 
+            Console.WriteLine("Please write a sentence.");
+            sentence = Console.ReadLine();
+            Console.WriteLine("This is the sentence to be modified: " + sentence);//Sentence to be modified is written here
+
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;//Changing the cultre name of the string to en-US
+
+            Console.WriteLine("\"{0}\" to lowercase: {1}", sentence, myTI.ToLower(sentence));
+            Console.WriteLine("\"{0}\" to uppercase: {1}", sentence, myTI.ToUpper(sentence));
+            Console.WriteLine("\"{0}\" to titlecase: {1}", sentence, myTI.ToTitleCase(sentence));
+
+            end = Console.ReadLine();
+            Console.WriteLine("Program now ending...");
+        }
+    }
+}
